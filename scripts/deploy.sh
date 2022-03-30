b8650e0eca2f7e7729a27a5cbef128fbc4195bb9
Marc Pan <pxy0592@gmail.com>
2022-03-30 06:41:46 -0700

    correct some content

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 # ------------------------------------------------------------------------------
 # gh-pages 部署脚本
-# @author Zhang Peng
+# @author Marc Pan
 # @since 2020/2/10
 # ------------------------------------------------------------------------------
 
@@ -34,11 +34,11 @@ if [[ ${GITHUB_TOKEN} && ${GITEE_TOKEN} ]]; then
     # ${GITHUB_TOKEN} 是 Github 私人令牌；${GITEE_TOKEN} 是 Gitee 私人令牌
     # ${GITHUB_TOKEN} 和 ${GITEE_TOKEN} 都是环境变量；travis-ci 构建时会传入变量
     git push --force --quiet "https://pxy0592:${GITHUB_TOKEN}@github.com/pxy0592/blog.git" gh-pages
-    git push --force --quiet "https://turnon:${GITEE_TOKEN}@gitee.com/turnon/blog.git" gh-pages
+    # git push --force --quiet "https://turnon:${GITEE_TOKEN}@gitee.com/turnon/blog.git" gh-pages
 else
     echo "使用 ssh 公钥部署 gh-pages"
     git push -f git@github.com:pxy0592/blog.git gh-pages
-    git push -f git@gitee.com:turnon/blog.git gh-pages
+    # git push -f git@gitee.com:turnon/blog.git gh-pages
 fi
 
 cd ${ROOT_DIR}
